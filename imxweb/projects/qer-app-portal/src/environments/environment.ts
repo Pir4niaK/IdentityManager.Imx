03a75ba875b7504868ccffb5f7ada3047159812e
b3nj-Axpo <135707135+b3nj-Axpo@users.noreply.github.com>
2024-09-24 01:24:22 +0200

    Update dev environment URL

--- imxweb/projects/qer-app-portal/src/environments/environment.ts
+++ imxweb/projects/qer-app-portal/src/environments/environment.ts
@@ -30,7 +30,7 @@
 
 export const environment = {
   production: false,
-  clientUrl: 'http://localhost:8182',
+  clientUrl: 'https://oneim.westeurope.cloudapp.azure.com/ApiServer',
   appName: 'qer-app-portal',
   appVersion: '1.0.0'
 };
